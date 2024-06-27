fdac014132331b70ec6baae06822634953cb591b
Jatin <jatinsehgalvision@gmail.com>
2024-06-27 23:36:33 +0530

    header has been added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ResumeComponent } from './resume/resume.component';
+import { WhyMeComponent } from './why-me/why-me.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'contact', component: ContactComponent},
+  { path: 'skills', component: SkillsComponent},
+  {path: 'profile', component: ProfileComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'portfolio', component: PortfolioComponent},
+  {path:"resume", component: ResumeComponent},
+  {path:"why-me", component: WhyMeComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HelloWorldComponent } from '../components/hello-world/hello-world.component';
 
 @Component({
   selector: 'app-root',
