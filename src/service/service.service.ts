19aa3d690e5094a2f9da4be9f84b091365e6f6e9
Jatin <jatinsehgalvision@gmail.com>
2024-06-30 11:12:13 +0530

    showed the experience and its design

---
+++ src/service/service.service.ts
@@ -0,0 +1,19 @@
+// src/service/service.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceService {
+  private jsonUrl = '../assets/experience.json';
+; // Correct path
+
+  constructor(private http: HttpClient) { }
+
+  getExperiences(): Observable<any> {
+    console.log("I am called");
+    return this.http.get('assets/experience.json');
+  }
+}
