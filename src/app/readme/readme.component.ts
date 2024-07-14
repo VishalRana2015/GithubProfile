129af831a9c2b5ef2215d4c9ee553939cd79039a
Vishal Rana <ranavishal2015@gmail.com>
2024-07-14 11:19:10 +0530

    Modified readmeComponent to depend upon parent component to pass src

--- src/app/readme/readme.component.ts
+++ src/app/readme/readme.component.ts
@@ -1,16 +1,15 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
-interface DialogData{
-  src:string ;
-}
 
+interface DialogData{ 
+  src: string ;
+}
 @Component({
   selector: 'app-readme',
   templateUrl: './readme.component.html',
   styleUrl: './readme.component.css'
 })
 export class ReadmeComponent {
-    src: string =""
-    constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData){}
-}
+  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+} 
